a54826fdfd527361ca06ab34c312e048696fdf33
liulishuang <794844674@qq.com>
2021-10-09 15:12:27 +0800

    element按需引入常用组件

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,52 @@
 import Vue from 'vue'
-import { Button, Link } from 'element-ui'
+import {
+  Row,
+  Col,
+  Button,
+  Link,
+  Icon,
+  Form,
+  FormItem,
+  Input,
+  Radio,
+  RadioGroup,
+  Checkbox,
+  CheckboxGroup,
+  Select,
+  Option,
+  DatePicker,
+  TimeSelect,
+  TimePicker,
+  Table,
+  TableColumn,
+  Pagination,
+  Tabs,
+  TabPane,
+  Dialog,
+  Tag,
+  Loading,
+  Message,
+  MessageBox
+} from 'element-ui'
 
 import './element-variables.scss'
 
-Vue.use(Button)
-Vue.use(Link)
+Vue.use(Row).use(Col)
+Vue.use(Button).use(Link).use(Icon)
+Vue.use(Form).use(FormItem)
+Vue.use(Input)
+Vue.use(Radio).use(RadioGroup)
+Vue.use(Checkbox).use(CheckboxGroup)
+Vue.use(Select).use(Option)
+Vue.use(DatePicker).use(TimeSelect).use(TimePicker)
+Vue.use(Table).use(TableColumn)
+Vue.use(Pagination)
+Vue.use(Tabs).use(TabPane)
+Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Loading.directive)
+Vue.prototype.$loading = Loading.service
+Vue.prototype.$message = Message
+const { alert, confirm } = MessageBox
+Vue.prototype.$alert = alert
+Vue.prototype.$confirm = confirm
