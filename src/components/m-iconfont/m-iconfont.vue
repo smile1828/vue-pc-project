fd23fe4f2459cfba48f08f60f820f1c660e81904
liulishuang <794844674@qq.com>
2021-10-11 11:44:48 +0800

    字体图标组件

---
+++ src/components/m-iconfont/m-iconfont.vue
@@ -0,0 +1,26 @@
+<template>
+    <span class="iconfont" :class="name" :style="[{color: color, fontSize: size}]" @click="$emit('click', $event)"></span>
+</template>
+
+<script>
+export default {
+  name: 'm-iconfont',
+  props: {
+    name: String,
+    color: String,
+    size: String
+  }
+}
+</script>
+
+<style scoped>
+@import url("./iconfont.css");
+.iconfont {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: inherit;
+}
+</style>
